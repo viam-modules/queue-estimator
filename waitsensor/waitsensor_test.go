449ffae27632e4a1a82678e7d723f604ddd7168d
Alan Davidson <alan@viam.com>
2025-01-22 16:47:28 -0500

    tests pass, but need more of them

--- waitsensor/waitsensor_test.go
+++ waitsensor/waitsensor_test.go
@@ -1,22 +1,31 @@
 package waitsensor
 
 import (
+	"fmt"
 	"testing"
 
 	"go.viam.com/test"
 )
 
-var validConfig = Config{DetectorName: "test",
+func makeValidConfig() Config {
+	return Config{DetectorName: "test",
 	                     CountPeriod: 5.0,
 	                     NSamples: 10,
+						 CountThresholds: map[string]int{
+							 "one": 1,
+							 "two": 2,
+							 "three": 3,
+						 },
 	                     ValidRegions: map[string][]BoundingBoxConfig{
-	                         "one": {XMin: 0.25,
-	                                 XMin: 0.25,
-	                                 YMax: 0.10,
-	                                 YMax: 0.90,
-	                                 },
+	                         "box": {{XMin: 0.25,
+	                                  XMax: 0.75,
+	                                  YMin: 0.10,
+	                                  YMax: 0.90,
+	                                  }},
 	                     },
 		        	 }
+				 }
+
 
 func TestValidateEmpty(t *testing.T) {
 	cfg := Config{}
@@ -26,15 +35,37 @@ func TestValidateEmpty(t *testing.T) {
 }
 
 func TestValidateCorrect(t *testing.T) {
+	validConfig := makeValidConfig()
 	_, err := validConfig.Validate("")
 	test.That(t, err, test.ShouldBeNil)
 }
 
 func TestValidateInvalidBoundingBox(t *testing.T) {
-	negativeBounds := validConfig
-	negativeBounds.ValidRegions["one"].XMin = -0.2
-	_, err := cfg.Validate("")
-	test.That(t, err, test.ShouldNotBeNil)
-	test.That(t, err.Error(), test.ShouldContainSubstring, "detector_name")
-}
+	var err error
 
+	negativeXBounds := makeValidConfig()
+	negativeXBounds.ValidRegions["box"][0].XMin = -0.2
+	_, err = negativeXBounds.Validate("")
+	test.That(t, err, test.ShouldNotBeNil)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "must be numbers between 0 and 1")
+
+	negativeYBounds := makeValidConfig()
+	negativeYBounds.ValidRegions["box"][0].YMin = -0.2
+	_, err = negativeYBounds.Validate("")
+	test.That(t, err, test.ShouldNotBeNil)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "must be numbers between 0 and 1")
+
+	incorrectOrderX := makeValidConfig()
+	incorrectOrderX.ValidRegions["box"][0].XMin = 0.99
+	_, err = incorrectOrderX.Validate("")
+	test.That(t, err, test.ShouldNotBeNil)
+	fmt.Println(incorrectOrderX)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "must be less than x_max")
+
+	incorrectOrderY := makeValidConfig()
+	incorrectOrderY.ValidRegions["box"][0].YMin = 0.99
+	_, err = incorrectOrderY.Validate("")
+	test.That(t, err, test.ShouldNotBeNil)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "must be less than y_max")
+
+}
