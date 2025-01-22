b1ca9d4640e2093feedc8a4301da27bf0a8a30a6
Alan Davidson <alan@viam.com>
2025-01-22 16:35:28 -0500

    start writing tests, pausing because they all hang

--- waitsensor/waitsensor_test.go
+++ waitsensor/waitsensor_test.go
@@ -6,9 +6,35 @@ import (
 	"go.viam.com/test"
 )
 
-func TestValidate(t *testing.T) {
+var validConfig = Config{DetectorName: "test",
+	                     CountPeriod: 5.0,
+	                     NSamples: 10,
+	                     ValidRegions: map[string][]BoundingBoxConfig{
+	                         "one": {XMin: 0.25,
+	                                 XMin: 0.25,
+	                                 YMax: 0.10,
+	                                 YMax: 0.90,
+	                                 },
+	                     },
+		        	 }
+
+func TestValidateEmpty(t *testing.T) {
 	cfg := Config{}
 	_, err := cfg.Validate("")
 	test.That(t, err, test.ShouldNotBeNil)
 	test.That(t, err.Error(), test.ShouldContainSubstring, "detector_name")
 }
+
+func TestValidateCorrect(t *testing.T) {
+	_, err := validConfig.Validate("")
+	test.That(t, err, test.ShouldBeNil)
+}
+
+func TestValidateInvalidBoundingBox(t *testing.T) {
+	negativeBounds := validConfig
+	negativeBounds.ValidRegions["one"].XMin = -0.2
+	_, err := cfg.Validate("")
+	test.That(t, err, test.ShouldNotBeNil)
+	test.That(t, err.Error(), test.ShouldContainSubstring, "detector_name")
+}
+
